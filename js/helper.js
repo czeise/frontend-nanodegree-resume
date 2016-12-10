25942c43cf50986a1b4df900bc8314c71b7d9b3b
Craig Zeise <craig.zeise@gmail.com>
2016-12-10 11:22:04 -0600

    Display map infoWindow on click

--- js/helper.js
+++ js/helper.js
@@ -178,6 +178,8 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+      // Improvement: display infoWindow on click!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
